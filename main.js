d77d30294c56a939ecccfd9ba4fb5aa848a4838d
Mrgr67 <116850941+Mrgr67@users.noreply.github.com>
2024-08-03 22:08:57 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Selamat malam Sayangku","maaf soal tadi" , "Aku Sayang Banget Sama Kamu", "I Love You"],
+        typeSpeed: 70
+    });
+}
